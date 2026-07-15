b3a99cb03c2f3dfb3db7ebc6f87668b26259f404
Mihirpatel6996 <96419193+Mihirpatel6996@users.noreply.github.com>
2026-07-15 09:15:36 +0530

    Time: 10 ms (58.15%) | Memory: 21 MB (72.69%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -1,52 +1,20 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        pos_arr = []
-        neg_arr =[]
-        #seperate positives and negatives
-        for i in range(n):
-            if nums[i] >= 0:
-                pos_arr.append(nums[i])
-            else:
-                neg_arr.append(nums[i])
-        
-        # now handle edge cases 
+        result = [0] *n
+        left, right = 0, n-1
+        pos = n-1
 
-        if(len(neg_arr) == 0):
-            nums[:] = [num**2 for num in nums] 
-            return nums
-        if(len(pos_arr) == 0):
-            nums[:]= [num **2 for num in nums]
-            nums.reverse()
-            return nums
-        
-        i =0 
-        j =0
-        m = len(pos_arr)
-        n = len(neg_arr)
-        id = 0 
-        result = []
-
-        pos_arr[:] = [num**2 for num in pos_arr]
-        neg_arr[:] = [num**2 for num in neg_arr]
-        neg_arr.reverse()
-        while (i<m and j<n):
-            if pos_arr[i] < neg_arr[j]:
-                result.append(pos_arr[i])
-                i+=1
+        while left <= right:
+            if abs(nums[left]) > abs(nums[right]):
+                result[pos] = nums[left]**2
+                left +=1
             else :
-                result.append(neg_arr[j])
-                j+=1
-        
-        while(i<m):
-            result.append(pos_arr[i])
-            i+=1
-        
-        while(j<n):
-            result.append(neg_arr[j])
-            j+=1
-        
+                result[pos] = nums[right]**2
+                right -= 1
+            pos -= 1
         return result
-
+        
+            
 
         
